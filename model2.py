07facd933e9d3a91db5e5507375c8ce0348ae3bd
Shotaro Oyama <shotaro12oyama@gmail.com>
2017-08-21 12:39:45 +0900

    track2

---
+++ model2.py
@@ -0,0 +1,120 @@
+import csv
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+from keras.layers import Cropping2D, Dense, Dropout, Flatten, Lambda
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.models import Model, Sequential
+from sklearn.model_selection import train_test_split
+
+samples = []
+with open('./data2/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        samples.append(line)
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+
+def generator(samples, batch_size):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './data2/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+                center_image = cv2.GaussianBlur(center_image, (3,3),0)                
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+                correction = 0.2 # this is a parameter to tune
+                left_angle = center_angle + correction
+                right_angle = center_angle - correction
+
+                name = './data2/IMG/'+batch_sample[1].split('/')[-1]
+                left_image = cv2.imread(name)
+                left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
+                left_image = cv2.GaussianBlur(left_image, (3,3),0)
+
+
+                name = './data2/IMG/'+batch_sample[2].split('/')[-1]
+                right_image = cv2.imread(name)
+                right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
+                right_image = cv2.GaussianBlur(right_image, (3,3),0)
+
+
+                # add images and angles to data set
+                images.append(left_image)
+                images.append(right_image)
+                angles.append(left_angle)
+                angles.append(right_angle)
+
+
+
+            augmented_images, augmented_angles = [], []
+            for image, angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_angles.append(angle*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=128)
+validation_generator = generator(validation_samples, batch_size=128)
+
+
+model = Sequential()
+# Preprocess incoming data, centered around zero with small standard deviation
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+
+#model.compile(loss='mse', optimizer='adam')
+#model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2, verbose=1)
+model.compile(loss='mse', optimizer='adam')
+history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=5, verbose=1)
+
+model.save('model2.h5')
+
+
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
+
+exit()
